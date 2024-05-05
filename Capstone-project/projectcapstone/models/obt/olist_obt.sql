f03c8b5cddf26e59409bf70829dcc5fb6e193f06
Fooklnwza007 <131597296+Fooklnwza007@users.noreply.github.com>
2024-05-05 14:49:55 +0000

    update sql

--- Capstone-project/projectcapstone/models/obt/olist_obt.sql
+++ Capstone-project/projectcapstone/models/obt/olist_obt.sql
@@ -1,9 +1,35 @@
 {{ config(materialized='table') }}
 
-with customers as (
+with geo as (
 
-    SELECT * 
-    FROM `project-capstone-422211.order.olist_customers_dataset` as c
+    select distinct *
+    from (select geolocation_zip_code_prefix,
+        geolocation_state
+        from `project-capstone-422211.order.olist_geolocation_dataset` as geo)
+),
+
+geo_new as (
+    
+    select geolocation_zip_code_prefix,
+    bs.State as geolocation_state,
+    bs.Latitude as geolocation_lat,
+    bs.Longitude as geolocation_lng,
+    from geo
+    left join `project-capstone-422211.order.brazil_state` as bs
+    on geolocation_state = bs.UF
+
+),
+customers as (
+
+    select distinct customer_id,
+    c.customer_zip_code_prefix,
+    customer_city,
+    geo.geolocation_state as customer_state,
+    geo.geolocation_lat as customer_lat,
+    geo.geolocation_lng as customer_long,
+    from `project-capstone-422211.order.olist_customers_dataset` as c
+    left join geo_new as geo
+    on c.customer_zip_code_prefix = geo.geolocation_zip_code_prefix
 
 ),
 
@@ -22,13 +48,13 @@ orders as (
     seller_id,
     shipping_limit_date,
     price
-    from `project-capstone-422211.order.olist_orders_dataset` as o
+    from `project-capstone-422211.order.partitioned_olist_orders_dataset` as o
     join `project-capstone-422211.order.olist_items_dataset` as oi on o.order_id = oi.order_id
 
 ),
 products as (
 
-    select *
+    select p.product_id, p.product_category_name, pc.product_category_name_english
     from `project-capstone-422211.order.olist_products_dataset` as p
     left join `project-capstone-422211.order.product_category_name_translation` as pc
     on p.product_category_name = pc.product_category_name
@@ -36,8 +62,15 @@ products as (
 ),
 sellers as (
 
-    select *
+    select distinct seller_id,
+    s.seller_zip_code_prefix,
+    seller_city,
+    geo.geolocation_state as seller_state,
+    geo.geolocation_lat as seller_lat,
+    geo.geolocation_lng as seller_long,
     from `project-capstone-422211.order.olist_sellers_dataset` as s
+    left join geo_new as geo
+    on s.seller_zip_code_prefix = geo.geolocation_zip_code_prefix
 
 ),
 
@@ -49,7 +82,9 @@ olist_obt as (
     o.order_approved_at,
     o.order_item_id,
     o.product_id,
-    p.product_category_name_english as product_category_name,
+    case
+    when p.product_category_name_english is null then p.product_category_name
+    else p.product_category_name_english end as product_category_name,
     o.price,
     o.order_delivered_carrier_date,
     o.order_delivered_customer_date,
@@ -58,16 +93,20 @@ olist_obt as (
     c.customer_zip_code_prefix,
     c.customer_city,
     c.customer_state,
+    c.customer_lat,
+    c.customer_long,
     o.seller_id,
     o.shipping_limit_date,
     s.seller_zip_code_prefix,
     s.seller_city,
     s.seller_state,
+    s.seller_lat,
+    s.seller_long,
 
     from orders as o 
-    join customers as c on c.customer_id = o.customer_id
+    left join customers as c on c.customer_id = o.customer_id
     left join products as p on p.product_id = o.product_id
-    join sellers as s on s.seller_id = o.seller_id
+    left join sellers as s on s.seller_id = o.seller_id
 
 )
 
--- Capstone-project/projectcapstone/models/obt/view_delivery_performance.sql
+++ Capstone-project/projectcapstone/models/obt/view_delivery_performance.sql
@@ -1,19 +1,37 @@
 select *,
 case
-    when num_of_delivered_date < 4 then "VERY FAST"
-	when num_of_delivered_date >= 4 and num_of_delivered_date < 8 then "FAST"
-	when num_of_delivered_date >= 8 and num_of_delivered_date < 15 then "NORMAL"
-	when num_of_delivered_date >= 15 and num_of_delivered_date < 22 then "SLOW"
-    when num_of_delivered_date is null then "-"
-	else "VERY SLOW"
-end as shipping_order_group,
+    when carrier_take < 4 then "Very Fast"
+	when carrier_take >= 4 and carrier_take < 8 then "Fast"
+	when carrier_take >= 8 and carrier_take < 15 then "Normal"
+	when carrier_take >= 15 and carrier_take < 22 then "Slow"
+    when carrier_take is null then "-"
+	else "Very Slow"
+end as carrier_take_bin,
+case
+    when ontinme_delivery < 0 then "On-time"
+    when ontinme_delivery >= 1 then "Late"
+    when ontinme_delivery is null then "-"
+    else "On-time"
+end as ontinme_delivery_bin
 from (select order_id,
     order_status,
-    order_purchase_timestamp,
-    order_delivered_customer_date,
-    order_delivered_carrier_date,
-    date_diff(order_delivered_customer_date, order_delivered_carrier_date, day) as num_of_delivered_date,
-    order_approved_at,
     product_id,
     product_category_name,
+    customer_lat,
+    customer_long,
+    customer_city,
+    customer_state,
+    seller_lat,seller_long,
+    seller_city,
+    seller_state,
+    order_purchase_timestamp,
+    order_approved_at,
+    date_diff(order_approved_at, order_purchase_timestamp, day) as approved_time,
+    order_delivered_carrier_date,
+    date_diff(order_delivered_carrier_date, order_approved_at, day) as seller_prepare,
+    order_estimated_delivery_date,
+    order_delivered_customer_date,
+    date_diff(order_delivered_customer_date, order_delivered_carrier_date, day) as carrier_take,
+    date_diff(order_delivered_customer_date, order_purchase_timestamp, day) as delivery_time,
+    date_diff(order_delivered_customer_date, order_estimated_delivery_date, day) as ontinme_delivery
 from {{ ref('olist_obt') }} )
